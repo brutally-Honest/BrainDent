1272351653a29b5fb2d7f9a11bf37c9fea90b395
karthik <karthikhs1402@gmail.com>
2024-01-27 13:19:51 +0530

    cleanup

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const configureDB = async () => {
   const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017";
-  const dbName = process.env.DB_NAME || "test-app";
+  const dbName = process.env.DB_NAME || "quiz-app";
   try {
     await mongoose.connect(`${dbUrl}/${dbName}`);
     console.log("Connected to db", dbName);
